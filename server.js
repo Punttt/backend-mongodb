7bed657126f510f72442c3091706564d80fe247a
Pontus Johansson <johanssonp998@gmail.com>
2026-04-23 16:52:05 +0200

    lägger till en app.listen för att lyssna efter port

--- server.js
+++ server.js
@@ -15,3 +15,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("Connected to MongoDB"))
     .catch((error) => console.log("Error connecting to database: " + error))
+
+
+
+// Startar servern och lyssnar efter porten
+app.listen(port, ()=> {
+    console.log("Server is running on port: " + port);
+});
