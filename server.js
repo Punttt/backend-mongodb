2094050856b98fb4b6cf021018a7bec0e58c77a9
Pontus Johansson <johanssonp998@gmail.com>
2026-04-23 16:47:36 +0200

    lägg in app use, och cors

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ require("dotenv").config();
 // Initierar express
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Tillåter JSON och cross origin CORS
+app.use(cors());
+app.use(express.json());
