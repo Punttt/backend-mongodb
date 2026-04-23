2f1c2c8a62cdef242f577a06dbe12dccb05726a4
Pontus Johansson <johanssonp998@gmail.com>
2026-04-23 16:46:33 +0200

    initierar express och port

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Initierar express
+const app = express();
+const port = process.env.PORT || 3000;
